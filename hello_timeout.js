403be5cd3cb96d62226b7c32caa5d6fc793dd7d5
Andy Junhyuk Park <pos03142ap@gmail.com>
2021-12-01 00:14:17 +0000

    first commit

---
+++ hello_timeout.js
@@ -0,0 +1,7 @@
+// Challenge #1
+// setTimeout(() => {console.log('4311o th3r3 w0r1d')}, 3000);
+
+// Challenge #2
+setTimeout(() => {console.log('4311o')}, 1000);
+setTimeout(() => {console.log('th3r3')}, 2000);
+setTimeout(() => {console.log('w0r1d')}, 3000);
